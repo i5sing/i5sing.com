b78b724ce5bd4443d2b1e7d1b0082ac5406017c9
miaowing <me@zf.ink>
2020-07-16 17:02:35 +0800

    update carousel image version

--- src/resources/images/carousel.js
+++ src/resources/images/carousel.js
@@ -1,35 +1,37 @@
 /**
  * Created by feng on 2017/3/31.
  */
+const version = '0.2.2';
+
 export default [
     {
         name: 0,
         url: 'javascript:void 0;',
-        img: 'http://static.i5sing.com/image/0.2.0/2.png'
+        img: 'http://static.i5sing.com/image/' + version + '/1.png'
     },
     {
         name: 1,
         url: 'javascript:void 0;',
-        img: 'http://static.i5sing.com/image/0.2.0/1.png'
+        img: 'http://static.i5sing.com/image/' + version + '/2.png'
     },
     {
         name: 2,
         url: 'javascript:void 0;',
-        img: 'http://static.i5sing.com/image/0.2.0/6.png'
+        img: 'http://static.i5sing.com/image/' + version + '/3.png'
     },
     {
         name: 3,
         url: 'javascript:void 0;',
-        img: 'http://static.i5sing.com/image/0.2.0/3.png'
+        img: 'http://static.i5sing.com/image/' + version + '/4.png'
     },
     {
         name: 4,
         url: 'javascript:void 0;',
-        img: 'http://static.i5sing.com/image/0.2.0/4.png'
+        img: 'http://static.i5sing.com/image/' + version + '/5.png'
     },
     {
         name: 5,
         url: 'javascript:void 0;',
-        img: 'http://static.i5sing.com/image/0.2.0/5.png'
+        img: 'http://static.i5sing.com/image/' + version + '/6.png'
     }
 ]
